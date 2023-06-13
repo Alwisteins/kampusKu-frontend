738494e21e1e2e44ce9c728fe56e14f4359ddc25
unknown <alwyyourbae@gmail.com>
2023-06-13 06:36:13 -0700

    fix(about.scss): fix hidden about page

--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -3,18 +3,18 @@ import Visi from "../../components/about-page/Visi";
 import Misi from "../../components/about-page/Misi";
 import Team from "../../components/about-page/Team";
 import Contact from "../../components/about-page/Contact";
+import "./about.scss";
 
 function About() {
   return (
-    <div>
+    <div className="about">
       <Navbar />
-      <h1>Tentang Kami</h1>
-      <p>Selamat datang di KampusKu!</p>
-
-      <Visi />
-      <Misi />
-      <Team />
-      <Contact />
+      <div className="section">
+        <Visi />
+        <Misi />
+        <Team />
+        <Contact />
+      </div>
     </div>
   );
 }
